66c243014053092370db735291d7a75ff884b36b
Eduardo Lizama <eduardo.lizama06@gmail.com>
2025-11-05 13:26:24 -0300

    docs(index):etapa 1

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 import chalk from 'chalk';
 
 // Mensajes iniciales
-console.log(chalk.blue('👋 ¡Bienvenido a la Terminal Parlante!'));
-console.info(chalk.green('ℹ️ Esta terminal está viva... y tiene estilo.'));
-console.warn(chalk.yellow('⚠️ Atención: Esto puede ponerse colorido.'));
-console.error(chalk.red('❌ Error simulado: algo salió demasiado bien.'));
-console.log(chalk.magenta('💡 Recuerda: la práctica hace al programador.'));
+//console.log(chalk.blue('👋 ¡Bienvenido a la Terminal Parlante!'));
+//console.info(chalk.green('ℹ️ Esta terminal está viva... y tiene estilo.'));
+//console.warn(chalk.yellow('⚠️ Atención: Esto puede ponerse colorido.'));
+//console.error(chalk.red('❌ Error simulado: algo salió demasiado bien.'));
+//console.log(chalk.magenta('💡 Recuerda: la práctica hace al programador.'));
 
 // BONUS: función básica
-function saludar(nombre) {
-  console.log(chalk.cyan(`Hola ${nombre}, ¡bienvenido al mundo JavaScript! 🌍`));
+//function saludar(nombre) {
+  //console.log(chalk.cyan(`Hola ${nombre}, ¡bienvenido al mundo JavaScript! 🌍`)); 
+//}
+function mostrarEstado(nombre) {
+  console.info(chalk.cyan(`El programa a iniciado! 🌍`)); 
 }
 
+mostrarEstado();
+
+
+
 // Prueba de función
-saludar("Equipo JS");
+//("Equipo JS");
